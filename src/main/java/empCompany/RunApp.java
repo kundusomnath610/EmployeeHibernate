30ec4250530ed68638af096637a5df67d881ffb2
Somnath Kundu <kundusomnath610@gmail.com>
2025-06-13 19:40:40 +0530

    commit

--- src/main/java/empCompany/RunApp.java
+++ src/main/java/empCompany/RunApp.java
@@ -18,14 +18,14 @@ public class RunApp {
 
         Company c = new Company();
 
-        Employee e1 = new Employee(303, "Priya", "dev", 32000, c);
-        Employee e2 = new Employee(403, "Anjali", "Testing", 34000, c);
+        Employee e1 = new Employee(100, "Priya", "dev", 32000, c);
+        Employee e2 = new Employee(200, "Anjali", "Testing", 34000, c);
 
         List<Employee> empList = new ArrayList<Employee>();
         empList.add(e1);
         empList.add(e2);
 
-        c.setcId(2);
+        c.setcId(1);
         c.setEmpList(empList);
 
         sess.persist(c);
